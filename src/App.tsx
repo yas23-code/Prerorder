764a16b8d9f7dd4a806a22f3fb5ad8eb6f352852
yas23-code <yashsaraswat834@gmail.com>
2025-12-11 23:45:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
 
           {/* Student Routes */}
           <Route path="/student" element={<StudentDashboard />} />
